5ed94f5aa9aab5dde83ebc75d451e2a96f42db2b
Rohith HB <rohithjnv96@gmail.com>
2023-06-25 05:48:11 +1000

    Adding post update and delete feature
    Among other minor improvements

--- on_blog/routes.py
+++ on_blog/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from PIL import Image
 
@@ -142,8 +142,41 @@ def new_post():
         db.session.commit()
         flash("Your post has been created", category="success")
         return redirect(url_for('home'))
-    return render_template('create_post.html', title="New Post", form = form)
+    return render_template('create_post.html', title="New Post", form = form, legend='Create Post ')
 
 
+@app.route('/post/<int:post_id>')
+@login_required
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
 
+@app.route('/post/<int:post_id>/update', methods=['POST', 'GET'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    elif form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updates', category='success')
+        return redirect(url_for('post', post_id= post.id))
+    return render_template('create_post.html', title="Update Post", form = form)
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST', 'GET'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.author:
+        db.session.delete(post)
+        db.session.commit()
+        flash("This post has been deleted successfully", category='success')
+    return redirect(url_for('home'))
 
